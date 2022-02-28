443396ab1f3c7569378fbf9bd3e530722a122fde
Paweł Albanowski <50399152+pawelalbanowski@users.noreply.github.com>
2022-02-28 07:19:07 +0100

    Update func.py

--- func.py
+++ func.py
@@ -1,13 +1,13 @@
 import json
 
-def json_read(file):
+def check_H(file):
     read_json_file = open(file, 'r')
     json_data = json.load(read_json_file)
     read_json_file.close()
     return json_data
 
 
-def json_write(file, data):
+def save_H(file, data):
     write_json = json.dumps(data, indent=4)
     write_file = open(file, 'w')
     write_file.write(write_json)
